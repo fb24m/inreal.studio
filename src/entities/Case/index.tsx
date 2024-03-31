383b1c2a5e403d8e235971a0e031f3e69fcc1282
--global <--global>
2024-03-31 14:09:12 +0400

    add suspense to case

--- src/entities/Case/index.tsx
+++ src/entities/Case/index.tsx
@@ -4,28 +4,31 @@ import styles from './Case.module.scss'
 import Link from 'next/link'
 import { Popup } from '@/shared/components/Popup'
 import { Title2 } from '@/shared/components/Title2'
+import { Suspense } from 'react'
 
 const Case = (props: CaseProps) => {
 	return (
-		<div className={props.className}>
-			<Link href={`?modal=${props.slug}`} className={styles.case}>
-				<WpImage className={styles.image} imageId={props.imageId} width={300} height={300} />
-				<div className={styles.footer}>
-					<h2 className={styles.title}>{props.title}</h2>
-					<span className={styles.year}>{props.year}</span>
-				</div>
-			</Link>
-			<Popup popupName={props.slug}>
-				<WpImage className={styles.popupImage} imageId={props.imageId} width={300} height={300} />
-				<span className={styles.category}>
-					{props.categoryName}
-				</span>
-				<Title2 className={styles.popupTitle}>{props.title}</Title2>
-				<div className={styles.content} dangerouslySetInnerHTML={{ __html: props.description }}>
+		<Suspense>
+			<div className={props.className}>
+				<Link href={`?modal=${props.slug}`} className={styles.case}>
+					<WpImage className={styles.image} imageId={props.imageId} width={300} height={300} />
+					<div className={styles.footer}>
+						<h2 className={styles.title}>{props.title}</h2>
+						<span className={styles.year}>{props.year}</span>
+					</div>
+				</Link>
+				<Popup popupName={props.slug}>
+					<WpImage className={styles.popupImage} imageId={props.imageId} width={300} height={300} />
+					<span className={styles.category}>
+						{props.categoryName}
+					</span>
+					<Title2 className={styles.popupTitle}>{props.title}</Title2>
+					<div className={styles.content} dangerouslySetInnerHTML={{ __html: props.description }}>
 
-				</div>
-			</Popup>
-		</div>
+					</div>
+				</Popup>
+			</div>
+		</Suspense>
 	)
 }
 
