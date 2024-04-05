319e90abb00aac83be4ef46e09efe7d42e90df71
--global <--global>
2024-04-05 12:43:14 +0400

    optimize home page animation

--- src/widgets/HomeBackground.tsx
+++ src/widgets/HomeBackground.tsx
@@ -5,9 +5,6 @@ import { useMedia } from '@/shared/hooks/useMedia.hook'
 import { useEffect } from 'react'
 
 export const HomeBackground = () => {
-	const pc = useMedia('(min-width: 992px)')
-
-
 	useEffect(() => {
 		const screens = document.querySelectorAll('.screen')
 		const html = document.querySelector('#home-screens')! as HTMLElement
@@ -19,15 +16,14 @@ export const HomeBackground = () => {
 		for (let i = screens.length - 1; i >= 0; i--) {
 			let screen = screens[i] as HTMLElement
 
-			screen.style.transition = `transform 1.2s, opacity .5s, max-height 1.2s, margin-top .85s`
-			screen.style.zIndex = `${i}`
+			screen.style.maxHeight = '0px'
 
 			const nextButton = document.querySelector('[data-next]')
 
 			if (nextButton) {
 				nextButton.addEventListener('click', () => {
 					window.scrollTo({
-						top: window.scrollY + screenHeight + 80,
+						top: window.scrollY + screenHeight + 10,
 						behavior: 'smooth'
 					})
 				})
@@ -36,60 +32,41 @@ export const HomeBackground = () => {
 			window.addEventListener('scroll', () => {
 				const thisScroll = window.scrollY - screenHeight * i
 
-				if (thisScroll >= 0 && thisScroll <= screenHeight) {
-					const image = screen.querySelector('img')
-					const video = screen.querySelector('video')
+				const image = screen.querySelector('img')
+				const video = screen.querySelector('video')
 
+				if (thisScroll >= 0 && thisScroll <= screenHeight) {
 					if (image) {
-						image.style.transform = `scale(${1 + (thisScroll / screenHeight) / 2}) translateY(${(thisScroll / screenHeight) * 5}px)`
-						image.style.marginTop = '0'
+						image.style.transform = `scale(${1 + (thisScroll / screenHeight)})`
+						image.removeAttribute('data-before-current')
 					}
 
 					if (video) {
-						video.style.transform = `scale(${1 + (thisScroll / screenHeight) / 2}) translateY(${(thisScroll / screenHeight) * 5}px)`
-						video.style.marginTop = '0'
+						video.style.transform = `scale(${1 + (thisScroll / screenHeight)})`
+						video.removeAttribute('data-before-current')
 					}
 
-					screen.style.opacity = '1'
-					screen.style.transform = `none`
 					screen.style.maxHeight = `${screenHeight}px`
-					screen.style.marginTop = `0px`
+
+					screen.removeAttribute('data-scrolled')
+					screen.setAttribute('data-before-scrolled', '')
 				}
 				else if (thisScroll <= screenHeight) {
-					screen.style.transform = `rotateZ(20deg) scale(.75)`
+					screen.setAttribute('data-scrolled', '')
+					screen.removeAttribute('data-before-scrolled')
+
 					screen.style.maxHeight = '0px'
 
-					const image = screen.querySelector('img')
-					const video = screen.querySelector('video')
-
-					if (pc) {
-
-						if (image) {
-							image.style.transform = 'scale(1.25) rotateZ(-20deg)'
-							image.style.marginTop = '-28%'
-						}
-						if (video) {
-							video.style.transform = 'scale(1.25) rotateZ(-20deg)'
-							video.style.marginTop = '-28%'
-						}
-
-						screen.style.marginTop = '40vh';
+					if (image) {
+						image.setAttribute('data-before-current', '')
 					}
-
-					else {
-						screen.style.transform = 'rotateZ(0)'
+					if (video) {
+						video.setAttribute('data-before-current', '')
 					}
-				}
-
-				if (screen.style.opacity === '1') {
-					screen.classList.add('_current')
-				}
-				else {
-					screen.classList.remove('_current')
 				}
 			})
 		}
-	}, [pc])
+	}, [])
 
 	return (
 		<div id="home-screens">
