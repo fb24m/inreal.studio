bc4f9ba2270334a58d0ca46e973ae84f24d0daee
fb24m <fakem1ner@yandex.ru>
2024-04-08 01:01:58 +0400

    increase horizontal tablet resolution

--- src/entities/HomeScreen/index.tsx
+++ src/entities/HomeScreen/index.tsx
@@ -13,8 +13,8 @@ export const HomeScreen = ({ label, image, first, video }: {
 	return (
 		<div className={clsx('screen', wrapper, first && top)}>
 			{video
-				&& <video className={imageEl} autoPlay muted loop preload="metadata"><source media="(min-width: 993px)" src={video} type="video/mp4" /></video>}
-			<Image width={2560} height={1080} src={image} alt="" className={clsx(imageEl, video && imgTag)} {...video && { media: "(max-width: 992px)" }} />
+				&& <video className={imageEl} autoPlay muted loop preload="metadata"><source media="(min-width: 1000px)" src={video} type="video/mp4" /></video>}
+			<Image width={2560} height={1080} src={image} alt="" className={clsx(imageEl, video && imgTag)} {...video && { media: "(max-width: 999px)" }} />
 			<h2 className={title}>{label}</h2>
 		</div>
 	)
